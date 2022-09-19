35700617b991407c21a25e60555ad25523d8b923
Maria Hansen <79523132+MariaMHansen@users.noreply.github.com>
2022-09-19 13:20:34 +0200

    answers removed

--- src/main/java/Assignment1Tasks.java
+++ src/main/java/Assignment1Tasks.java
@@ -1,17 +1,13 @@
 public class Assignment1Tasks {
     // Write a public static function `sayHello` that returns "hello world!"
-    // Add Your Code Here ...
     public static String sayHello(){
-            return "hello world!";
+        // Add Your Code Here ...
+        return null;
         }
+
+    // Write a public static function that counts how many times the letter combination "dog" appears in the string
     public static int countDogs(String phrase) {
-        int count = 0;
-        for(int i = 0; i < phrase.length()-2; i ++){
-            String sub = phrase.substring(i,i+3);
-            if("dog".equals(sub)){
-                count++;
-            }
-        }
-        return count;
+        // Add Your Code Here ...
+        return -1;
     }
-    }
+}
